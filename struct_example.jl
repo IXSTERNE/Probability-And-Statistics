2a261e1314951b6b9434076f7db1eef4d7f73248
xjulies <xjulies@xjuliess-MacBook-Air.local>
2023-11-21 10:36:53 +0800

    bothersome

--- struct_example.jl
+++ struct_example.jl
@@ -1,38 +1,34 @@
 using Distributions
 
-    struct PdfBinomial
+struct PdfBinomial
+# Honestly, think we can inherit functions?
 
-        number_of_trials::Int64
-        random_variable::Float64
+    #Specified types
+    number_of_trials::Int64
+    random_variable::Float64
 
-        function PdfWithSingleRandomVariable(number_of_trials, random_variable)
-            _probability = Binomial(number_of_trials, random_variable)
-            pmf = pdf(_probability, random_variable)
-            return pmf
-        end
-
-        function PdfWithRange(number_of_trials, random_variable, range)
-            # This one is supposed to take additional arguments: range
-            # Also creates a new array
-            array1 = []
-            _probability = Binomial(number_of_trials, random_variable)
-            for random_variable in range
-                pmf = pdf(_probability, random_variable)
-                push!(array1, pmf)
-            return array1
-        end
+    function PdfBinomial(number_of_trials::Int64, random_variable::Float64)
+        new(number_of_trials, random_variable)
     end
 
-    obj1 = PdfBinomial
-    obj1.PdfWithSingleRandomVariable(10, 5)
-    println(obj1)
+    PdfWithSingleRandomVariable = function(self::PdfBinomial)
+        _probability = Binomial(self.number_of_trials, self.random_variable)
+        pmf = pdf(_probability, self.random_variable)
+        return pmf
+    end
 
-    obj2 = PdfBinomial
-    obj2.PdfWithRange(10, 5, 20)
-    println(obj2)
+    PdfWithRange = function(self::PdfBinomial, range)
+        array1 = Float64[]
+        _probability = Binomial(self.number_of_trials, self.random_variable)
+        for random_variable in range
+            pmf = pdf(_probability, random_variable)
+            push!(array1, pmf)
+        end
+        return array1
+    end
 end
-# Keep this in check since this is probably wrong
-# Subject to change
 
-# Also if you just look at the old function file. It's starting to look the same
-# Upcoming matrix and multidimensional distribution density calculating.
+pdf_binomial = PdfBinomial(10, 0.5)
+
+result_single = pdf_binomial.PdfWithSingleRandomVariable(pdf_binomial)
+println(result_single)
